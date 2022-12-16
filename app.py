146e8791c0a008659e2970353dd266340993f337
MineMathijs <mathijs.wijkmans@gmail.com>
2022-12-16 12:24:30 +0100

    FLASK

--- app.py
+++ app.py
@@ -7,4 +7,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    return render_template("menu.html")
+
+
+@app.route('/invoer')
+def about():
+    return render_template("menu2.html")
+
+
+@app.route('/laad')
+def laad():
     return render_template("laad.html")
