0c024592ca5b1aca8b591c98a584d8ba841fa3e0
MineMathijs <mathijs.wijkmans@gmail.com>
2023-01-19 00:46:34 +0100

    Update algorithm.py

--- algorithm.py
+++ algorithm.py
@@ -1,5 +1,6 @@
 import numpy as np
-from random import randint, shuffle
+from random import randint, shuffle, choice, random
+import copy
 
 
 def leegRooster(dagen: int, uuren: int):
@@ -7,7 +8,6 @@ def leegRooster(dagen: int, uuren: int):
 
 
 def vulRooster(leegrooster: np.array, vakken: np.array):
-
     rooster = np.ravel(leegrooster)
     options = np.arange(rooster.size)
     shuffle(options)
@@ -24,30 +24,119 @@ def vulRooster(leegrooster: np.array, vakken: np.array):
     return rooster
 
 
+def calcFitness(startFitness, rooster):
+    fitness = startFitness
+    for i, dag in enumerate(rooster):
+        for j, uur in enumerate(dag):
+            if uur in dag[j+1:] and uur != 0:
+                if uur in dag[j+2:]:
+                    # print(f"2x in een dag op {i} : {j} met {uur}")
+                    fitness -= 100
+                if uur in [dag[j+1]]:
+                    fitness += 50
+                    # print(f"blokuur op {i} : {j} met {uur}")
+            if uur == 0 and np.count_nonzero(dag[:j]) != 0 and np.count_nonzero(dag[j+1:]):
+                # print(f"tussenuur op {i} : {j} met {uur}")
+                fitness -= 25
+    return fitness
+
+
+def sortArr(arr):
+    sortedArr = arr[arr[:, 1].argsort()]
+    sortedReverse = np.flip(sortedArr, axis=0)
+    return sortedReverse
+
+
+def crossover(parent1, parent2):
+    # Create copies of the parent
+    offspring = np.array([])
+    parent2_copy = np.copy(np.ravel(parent2))
+
+    # Create a dictionary of unique elements and their counts in the first parent
+    element_counts_parent1 = dict(zip(*np.unique(parent1, return_counts=True)))
+    # Randomly select an index to start from in the second parent
+    start_index = randint(0, parent2_copy.shape[0]-1)
+    # Iterate through the second parent starting from the selected index
+    for i in range(start_index, parent2_copy.shape[0]):
+        if element_counts_parent1.get(parent2_copy[i], 0) > 0:
+            element_counts_parent1[parent2_copy[i]] -= 1
+            offspring = np.append(offspring, parent2_copy[i])
+    # If we still need more elements, iterate from the beginning of the second parent
+    for i in range(0, start_index):
+        if element_counts_parent1.get(parent2_copy[i], 0) > 0:
+            element_counts_parent1[parent2_copy[i]] -= 1
+            offspring = np.append(offspring, parent2_copy[i])
+    offspring = offspring.reshape(parent1.shape)
+    return offspring
+
+
+def mutate(arr):
+    rows, cols = arr.shape
+    x1, y1 = np.random.randint(0, rows), np.random.randint(0, cols)
+    x2, y2 = np.random.randint(0, rows), np.random.randint(0, cols)
+    temp = arr[x1, y1]
+    arr[x1, y1] = arr[x2, y2]
+    arr[x2, y2] = temp
+    return arr
+
+
 def geneticAlgorithm(eenRooster):
     vakken = eenRooster[2]
     leegRooster = eenRooster[1]
-    populationSize = 500
+    populationSize = 1000
     fitnesStop = 1500
+    selectBest = 50
     maxGenerations = 200
     startFitness = 1000
+    crossoverSize = 500
+    mutateChance = 0.85
+    mutateAmmount = 2
+    mutateOnlySize = 150
 
-    population = np.array([[vulRooster(leegRooster, vakken), startFitness]
+    population = np.array([[vulRooster(copy.copy(leegRooster), vakken), startFitness]
                           for _ in range(populationSize)], dtype=object)
-    print(population)
+    # print(population)
 
     for i, rooster in enumerate(population):
         fitness = calcFitness(startFitness, rooster[0])
         population[i, 1] = fitness
-    return population[0]
 
-# def calcFitness(startFitness,rooster):
-#     fitness = startFitness
-#     for i, dag in enumerate(rooster):
-#         for j, uur in enumerate(dag):
-#             if uur in dag[:j-1]+dag[j+1:]:
-#                 print("2x in een dag")
-#                 fitness -= 50
+    population = sortArr(population)
+    generation = 0
+    print(f"best solution for generation {generation}: \n {population[0]}")
+
+    while population[0, 1] <= fitnesStop and generation <= maxGenerations:
+        lastbest = copy.deepcopy(population[:selectBest])
+        newPopulation = np.array([row[0] for row in lastbest])
+
+        for _ in range(crossoverSize):
+            parent1 = lastbest[randint(0, len(lastbest)-1), 0]
+            parent2 = lastbest[randint(0, len(lastbest)-1), 0]
+            child = crossover(parent1, parent2)
+            for _ in range(mutateAmmount):
+                if random() <= mutateChance:
+                    child = mutate(child)
+            newPopulation = np.append(newPopulation, [child], 0)
+
+        for _ in range(mutateOnlySize):
+            child = lastbest[randint(0, len(lastbest)-1), 0]
+            for _ in range(mutateAmmount):
+                if random() <= mutateChance:
+                    child = mutate(child)
+            newPopulation = np.append(newPopulation, [child], 0)
+
+        wildcards = populationSize - len(newPopulation)
+        for _ in range(wildcards):
+            child = vulRooster(copy.copy(leegRooster), vakken)
+            newPopulation = np.append(newPopulation, [child], 0)
+
+        population = np.array([[gene, calcFitness(startFitness, gene)]
+                               for gene in newPopulation], dtype=object)
+        population = sortArr(population)
+        generation += 1
+        print(f"best solution for generation {generation}: \n {population[0]}")
+
+    return population[0]
 
 
 if __name__ == "__main__":
